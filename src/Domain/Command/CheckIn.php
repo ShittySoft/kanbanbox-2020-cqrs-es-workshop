860e7c7b61df1aa0f5b910a294db8ad6eeb6d1ff
Marco Pivetta <ocramius@gmail.com>
2020-11-26 14:17:29 +0100

    Added recording of users checking into buildings

---
+++ src/Domain/Command/CheckIn.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Building\Domain\Command;
+
+use Prooph\Common\Messaging\Command;
+use Rhumsaa\Uuid\Uuid;
+
+final class CheckIn extends Command
+{
+    private Uuid $building;
+    private string $username;
+
+    private function __construct(Uuid $building, string $username)
+    {
+        $this->init();
+
+        $this->building = $building;
+        $this->username = $username;
+    }
+
+    public static function fromBuildingAndUsername(Uuid $building, string $username) : self
+    {
+        return new self($building, $username);
+    }
+
+    public function building() : Uuid
+    {
+        return $this->building;
+    }
+
+    public function username() : string
+    {
+        return $this->username;
+    }
+
+    /** {@inheritDoc} */
+    public function payload() : array
+    {
+        return [
+            'building' => $this->building->toString(),
+            'username' => $this->username,
+        ];
+    }
+
+    /** {@inheritDoc} */
+    protected function setPayload(array $payload)
+    {
+        $this->building = Uuid::fromString($payload['username']);
+        $this->username = $payload['username'];
+    }
+}
